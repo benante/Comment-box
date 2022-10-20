d901a6afefef1988419c651671c878903c25ce80
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2022-10-20 18:21:59 +0100

    charcount working

--- app.js
+++ app.js
@@ -6,7 +6,17 @@ function getData (event) {
 
     let newComment = document.createElement("li")
     newComment.innerHTML = textarea
-    document.getElementById("singleComment").append(newComment) 
-        
-    
+    document.getElementById("singleComment").append(newComment)    
 }
+
+textarea.addEventListener("input" , function charCount () {
+    let maxNum = 140
+    let currentNum = document.getElementById("textarea").value
+    currentNum = currentNum.length;
+    document.getElementById("charcount").innerHTML = currentNum + "/140";
+    if (currentNum > maxNum) {
+        document.getElementById("textarea").classList.add("wrong")
+    } else {
+        document.getElementById("textarea").classList.remove("wrong")
+    }
+})
