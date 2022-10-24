ee24c42a94971747077ffadb9320abd2c2cfe614
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2022-10-24 19:31:37 +0100

    Second commit, no more GetData(), replaced by addEventList

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-function getData (event) {
+let maxNum = 140
+document.addEventListener("submit" , function (event){
     event.preventDefault()
     let FullName = document.getElementById("FullName").value
     let email = document.getElementById("email").value
     let textarea = document.getElementById("textarea").value
+    if (textarea.length > maxNum) {
+        window.alert("Comment length exceeded")
+        textarea = textarea.slice(0,maxNum)
+    }
 
     let newComment = document.createElement("li")
     newComment.innerHTML = textarea
-    document.getElementById("singleComment").append(newComment)    
-}
+    document.getElementById("singleComment").append(newComment)
+})
+
+// function getData (event) {
+    // console.log(event)
+        
+// }
 
 textarea.addEventListener("input" , function charCount () {
-    let maxNum = 140
     let currentNum = document.getElementById("textarea").value
     currentNum = currentNum.length;
     document.getElementById("charcount").innerHTML = currentNum + "/140";
